87e96627738512b65a7578838fda7b3846dd7641
taotao7 <moca_tao7@foxmail.com>
2025-03-01 16:03:41 +0800

    chore: update .gitignore and rename event handler

--- src/main/action/index.ts
+++ src/main/action/index.ts
@@ -2,7 +2,7 @@ import { BrowserWindow, ipcMain } from 'electron'
 
 export default (window: BrowserWindow) => {
   console.log('window--->', window)
-  ipcMain.on('sip-action', (_, action, phone) => {
+  ipcMain.on('action-event', (_, action, phone) => {
     console.log('action--->', action, phone)
   })
 }
